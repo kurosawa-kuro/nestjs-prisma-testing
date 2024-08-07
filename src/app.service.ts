e13d6cc5074724c813a6fa5eeba1a87e4f21f417
Toshifumi Kurosawa <youremail@example.com>
2024-08-07 22:20:40 +0900

    a

--- src/app.service.ts
+++ src/app.service.ts
@@ -7,7 +7,6 @@ export class AppService {
 
   async getDatabaseConnectionStatus(): Promise<string> {
     try {
-      // データベースに対して簡単なクエリを実行
       await this.prisma.$queryRaw`SELECT 1`;
       return 'データベース接続に成功しました！';
     } catch (error) {
