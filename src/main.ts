bc2a93ff966f5d5b0e6008aebaa4445ce34de10d
Toshifumi Kurosawa <youremail@example.com>
2024-08-08 07:10:41 +0900

    setupSwagger

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 import { corsConfig } from './config/cors.config';
+import { setupSwagger } from './config/swagger.config';
 
 async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule);
 
   app.enableCors(corsConfig);
+  setupSwagger(app);
 
   const port = process.env.BACKEND_PORT || 8080;
   await app.listen(port);
   console.log(`Application is running on: http://localhost:${port}`);
+  console.log(`Swagger documentation is available at: http://localhost:${port}/api`);
 }
 bootstrap();
