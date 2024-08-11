dc2d24e1735d2dd0ee755a57758620a6362f4a74
Toshifumi Kurosawa <youremail@example.com>
2024-08-12 02:44:24 +0900

    a

--- prisma/migrations/20240811174313_/migration.sql
+++ prisma/migrations/20240811174313_/migration.sql
@@ -4,6 +4,10 @@ CREATE TABLE "User" (
     "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
+    "avatar" TEXT,
+    "role" TEXT NOT NULL DEFAULT 'user',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
