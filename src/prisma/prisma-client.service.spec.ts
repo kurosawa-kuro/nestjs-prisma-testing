ca8871818aab0eb10aeaec806bcdb9325f2db9c8
Toshifumi Kurosawa <youremail@example.com>
2024-08-14 20:52:18 +0900

    t

--- src/prisma/prisma-client.service.spec.ts
+++ src/prisma/prisma-client.service.spec.ts
@@ -7,15 +7,27 @@ describe('PrismaClientService', () => {
 
   beforeEach(async () => {
     service = new PrismaClientService();
-    jest.spyOn(service, '$connect'); // PrismaClient の $connect メソッドをスパイします
+    jest.spyOn(service, '$connect').mockResolvedValue(undefined); // 解決値を undefined に設定
+    jest.spyOn(service, '$disconnect').mockResolvedValue(undefined); // 解決値を undefined に設定
+    jest.spyOn(service, 'cleanDatabase').mockResolvedValue([]); // cleanDatabase が解決されるとき、空の配列を返すように設定
   });
 
   afterEach(async () => {
-    jest.clearAllMocks(); // モックをクリア
+    jest.clearAllMocks();
   });
 
   it('should connect when module is initialized', async () => {
-    await service.onModuleInit(); // onModuleInit を実行
-    expect(service.$connect).toHaveBeenCalled(); // $connect が呼ばれたかを検証
+    await service.onModuleInit();
+    expect(service.$connect).toHaveBeenCalled();
+  });
+
+  it('should disconnect when module is destroyed', async () => {
+    await service.onModuleDestroy();
+    expect(service.$disconnect).toHaveBeenCalled();
+  });
+
+  it('should clean the database before module is destroyed', async () => {
+    await service.cleanDatabase(); // cleanDatabase を実行
+    expect(service.cleanDatabase).toHaveBeenCalled(); // cleanDatabase が呼ばれたかを検証
   });
 });
