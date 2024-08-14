314ed76d0004912902650412191136cb3dfbe3c1
Toshifumi Kurosawa <youremail@example.com>
2024-08-14 21:43:14 +0900

    あ

--- src/prisma/prisma-client.service.ts
+++ src/prisma/prisma-client.service.ts
@@ -15,19 +15,4 @@ export class PrismaClientService
   async onModuleDestroy() {
     await this.$disconnect();
   }
-
-  async cleanDatabase() {
-    const models = Object.keys(this).filter((key) => {
-      return (
-        typeof this[key] === 'object' &&
-        this[key] !== null &&
-        'deleteMany' in this[key] &&
-        typeof this[key].deleteMany === 'function' &&
-        // モデルのプロパティをより明確に識別するための追加のチェック
-        Reflect.has(this[key], 'findFirst') // 通常、Prisma モデルは findFirst メソッドを持っています
-      );
-    });
-
-    return Promise.all(models.map((model) => this[model].deleteMany()));
-  }
 }
